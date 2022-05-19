30e3f4e9627ac7eb99857653f127a82e46d828fb
lsw0507 <87456847+lsw0507@users.noreply.github.com>
2022-05-19 19:19:28 +0900

    REST API

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,24 +1,13 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 
+const api = require('./api');
+
 const app = new Koa();
 const router = new Router();
 
-router.get('/', ctx => {
-  ctx.body = '홈';
-});
-
-router.get('/about/:name?', ctx => {
-  const { name } = ctx.params;
-  // name의 존재 유무에 따라 다른 결과 출력
-  ctx.body = name ? `${name}의 소개` : '소개';
-});
-
-router.get('/posts', ctx => {
-  const { id } = ctx.query;
-  // id의 존재 유무에 따라 다른 결과 출력
-  ctx.body = id ? `포스트#${id}` : '포스트 아이다가 없습니다.';
-})
+// 라우터 설정
+router.use('/api', api.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
 
