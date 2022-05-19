b13fe5265c7f207074d0a7daa3fcc42f3d9393d1
lsw0507 <87456847+lsw0507@users.noreply.github.com>
2022-05-19 17:37:52 +0900

    authorized Middleware Practice

---
+++ blog/blog-backend/src/index.js
@@ -0,0 +1,27 @@
+const koa = require('koa');
+
+const app = new koa();
+
+app.use((ctx, next) => {
+  console.log("ctx.url", ctx.url);
+  console.log(1);
+  if (ctx.query.authorized !== '1') {
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+  next();
+});
+
+app.use((ctx, next) => {
+  console.log(2);
+  next();
+});
+
+app.use((ctx) => {
+  ctx.body = 'hello world!';
+})
+
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+})
