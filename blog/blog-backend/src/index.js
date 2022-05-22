653c5b27bac12b0ee96848927d7356c4ab6f0b94
lsw0507 <87456847+lsw0507@users.noreply.github.com>
2022-05-22 23:51:12 +0900

    koa-router REST API

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const api = require('./api');
 
@@ -9,6 +10,10 @@ const router = new Router();
 // 라우터 설정
 router.use('/api', api.routes());
 
+// 라우터 적용 전에 bodyparser 적용
+app.use(bodyParser());
+
+// app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
