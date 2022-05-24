23d3658155596aac331c9965a5a7199e37a56fc5
lsw0604 <57swji@naver.com>
2022-05-24 21:07:10 +0900

    context Slice2

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -73,12 +73,11 @@ export const list = async ctx => {
       .sort({ _id: -1 })
       .limit(10)
       .skip((page - 1) * 10)
+      .lean()
       .exec();
     const postCount = await Post.countDocuments().exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10));
-    ctx.body = posts
-      .map(post => post.toJSON())
-      .map(post => ({
+    ctx.body = posts.map(post => ({
         ...post,
         body:
           post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
