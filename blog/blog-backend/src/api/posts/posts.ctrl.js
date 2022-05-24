6133115513ef246d56a3ffc8abd31d3ad0f82a48
lsw0604 <57swji@naver.com>
2022-05-24 20:31:48 +0900

    mongoose Patch Validate

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -39,7 +39,7 @@ export const write = async ctx => {
     ctx.body = result.error;
     return;
   }
-  
+
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
@@ -111,13 +111,28 @@ export const remove = async ctx => {
 
 export const update = async ctx => {
   const { id } = ctx.params;
+  // write에서 사용한 schema와 비슷한데, required()가 없습니다.
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  // 검증하고 나서 검증 실패인 경우 에러 처리
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400; // Bad Request
+    ctx.body = result.error;
+    return;
+  }
+
   try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
       // false일 때는 업데이트되기 전의 데이터를 반환합니다.
     }).exec();
     if (!post) {
-      ctx.status = 404
+      ctx.status = 404;
       return;
     } 
     ctx.body = post;
