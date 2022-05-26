e06b02ef46a95aab1bf1e2c49d9b13d2cbf1c68b
lsw0604 <57swji@naver.com>
2022-05-26 20:56:56 +0900

    containers AuthForm

--- blog/blog-frontend/src/components/auth/AuthForm.js
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -59,18 +59,26 @@ const textMap = {
   register: 'Register',
 };
 
-const AuthForm = ({ type }) => {
+const AuthForm = ({ type, form, onChange, onSubmit }) => {
   const text = textMap[type];
   return (
     <AuthFormBlock>
       <h3>{text}</h3>
-      <form>
-        <StyledInput autoComplete='username' placeholder="ID" />
+      <form onSubmit={onSubmit}>
+        <StyledInput 
+          autoComplete='username' 
+          name="username"
+          placeholder="ID" 
+          onChange={onChange}
+          value={form.username}
+        />
         <StyledInput
           autoComplete='new-password'
           name="password"
           placeholder='password'
           type='password'
+          onChange={onChange}
+          value={form.password}
         />
         {type === 'register' && (
           <StyledInput
@@ -78,6 +86,8 @@ const AuthForm = ({ type }) => {
             name="passwordConfirm"
             placeholder='password check'
             type='password'
+            onChange={onChange}
+            value={form.passwordConfirm}
           />
         )}
         <ButtonWithMarginTop cyan fullWidth style={{ marginTop: '1rem' }}>
