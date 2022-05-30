64d06aa7d0a348f1c5ea8a1fad8e4dfc451f7c25
lsw0604 <57swji@naver.com>
2022-05-31 00:16:27 +0900

    Header

--- blog/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-frontend/src/pages/PostListPage.js
@@ -1,9 +1,10 @@
-import Button from '../components/common/Button';
+import Header from '../components/common/Header';
 
 const PostListPage = () => {
   return (
     <div>
-      <Button>Button</Button>
+      <Header />
+      <div>HEllO</div>
     </div>
   );
 };
