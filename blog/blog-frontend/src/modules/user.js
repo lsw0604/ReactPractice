dbed3e143ee260cb19c2306123293e3a9481818a
lsw0604 <57swji@naver.com>
2022-05-31 11:59:37 +0900

    Login CheckFailure

--- blog/blog-frontend/src/modules/user.js
+++ blog/blog-frontend/src/modules/user.js
@@ -5,7 +5,9 @@ import createRequestSaga, {
   createRequestActionTypes,
 } from '../lib/createRequestSaga';
 
-const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그인 처리
+// 새로고침 이후 임시 로그인 처리
+const TEMP_SET_USER = 'user/TEMP_SET_USER'; 
+
 // 회원 정보 확인
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
   'user/CHECK',
@@ -15,8 +17,18 @@ export const tempSetUser = createAction(TEMP_SET_USER, user => user);
 export const check = createAction(CHECK);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
+
+function checkFailureSaga() {
+  try {
+    localStorage.removeItem('user');
+  } catch (e) {
+    console.log('localStorage is not working');
+  }
+}
+
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
+  yield takeLatest(CHECK_FAILURE, checkFailureSaga);
 };
 
 const initialState = {
