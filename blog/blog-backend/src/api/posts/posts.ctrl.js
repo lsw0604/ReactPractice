5106b8b49a5582d1f7d9d10a7ae82059fbd4ea6f
lsw0604 <57swji@naver.com>
2022-06-14 16:23:09 +0900

    post sanitzieHtml update, write

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,7 +1,7 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
 import Joi from 'joi';
-import sanitize from 'sanitize-html';
+import sanitizeHtml from 'sanitize-html';
 
 const { ObjectId } = mongoose.Types;
 
@@ -66,7 +66,7 @@ export const checkOwnPost = (ctx, next) => {
     tags: ['태그1', '태그2']
   }
 */
-export const write = async (ctx) => {
+export const write = async ctx => {
   const schema = Joi.object().keys({
     // 객체가 다음 필드를 가지고 있음을 검증
     title: Joi.string().required(), // required() 가 있으면 필수 항목
@@ -85,7 +85,7 @@ export const write = async (ctx) => {
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
-    body,
+    body: sanitizeHtml(body, sanitizeOption),
     tags,
     user: ctx.state.user,
   });
@@ -98,7 +98,7 @@ export const write = async (ctx) => {
 };
 
 const removeHtmlAndShorten = body => {
-  const filtered = sanitize(body, {
+  const filtered = sanitizeHtml(body, {
     allowedTags: [],
   });
   return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
@@ -187,8 +187,14 @@ export const update = async (ctx) => {
     return;
   }
 
+  const nextData = { ...ctx.request.body }; // 객체를 복사하고
+  // body 값이 주어졌으면 필터링
+  if (nextData.body) {
+    nextData.body = sanitizeHtml(nextData.body, sanitizeOption);
+  }
+
   try {
-    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+    const post = await Post.findByIdAndUpdate(id, nextData, {
       new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
       // false 일 때에는 업데이트 되기 전의 데이터를 반환합니다.
     }).exec();
