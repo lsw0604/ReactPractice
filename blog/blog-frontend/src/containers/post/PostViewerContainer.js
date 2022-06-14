a8c0c37f009c3a8bc4ba8bd2e5b877a0e2e22dc0
lsw0604 <57swji@naver.com>
2022-06-14 19:26:30 +0900

    PostViewerContainer EditButton

--- blog/blog-frontend/src/containers/post/PostViewerContainer.js
+++ blog/blog-frontend/src/containers/post/PostViewerContainer.js
@@ -3,17 +3,23 @@ import { useDispatch, useSelector } from 'react-redux';
 import { readPost, unloadPost } from '../../modules/post';
 import PostViewer from '../../components/post/PostViewer';
 import PostActionButtons from '../../components/post/PostActionButton';
-import { useParams } from 'react-router-dom';
+import { setOriginalPost } from '../../modules/write';
+import { removePost } from '../../lib/api/posts';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const PostViewerContainer = () => {
   // 처음 마운트될 때 포스트 읽기 API 요청
   const { postId } = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { post, error, loading } = useSelector(({ post, loading }) => ({
-    post: post.post,
-    error: post.error,
-    loading: loading['post/READ_POST'],
-  }));
+  const { post, error, loading, user } = useSelector(
+    ({ post, loading }) => ({
+      post: post.post,
+      error: post.error,
+      loading: loading['post/READ_POST'],
+      user: user.user,
+    }),
+  );
 
   useEffect(() => {
     dispatch(readPost(postId));
@@ -23,12 +29,21 @@ const PostViewerContainer = () => {
     };
   }, [dispatch, postId]);
 
+  const onEdit = () => {
+    dispatch(setOriginalPost(post));
+    navigate('/write');
+  };
+
+  const ownPost = (user && user._id) === (post && post.user._id);
+
   return (
     <PostViewer
       post={post}
       loading={loading}
       error={error}
-      actionButtons={<PostActionButtons />}
+      actionButtons={
+        ownPost && <PostActionButtons onEdit={onEdit} />
+      }
     />
   );
 };
