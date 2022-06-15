a6e76081ae8fd59d726792113548fddab03e4592
lsw0604 <57swji@naver.com>
2022-06-15 16:41:48 +0900

    Helmet

--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -2,14 +2,20 @@ import Responsive from "../components/common/Responsive";
 import EditorContainer from "../containers/write/EditorContainer";
 import TagBoxContainer from "../containers/write/TagBoxContainer";
 import WriteActionButtonContainer from "../containers/write/WriteActionButtonContainer";
+import { Helmet } from "react-helmet-async";
 
 const WritePage = () => {
   return (
-    <Responsive>
-      <EditorContainer />
-      <TagBoxContainer />
-      <WriteActionButtonContainer />
-    </Responsive>
+    <div>
+      <Helmet>
+        <title>POST WRITE - REACTERS</title>
+      </Helmet>
+      <Responsive>
+        <EditorContainer />
+        <TagBoxContainer />
+        <WriteActionButtonContainer />
+      </Responsive>
+    </div>
   );
 };
 
